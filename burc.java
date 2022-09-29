7ffb219528e22f74916e8a6f10aefbc0a04c8f4f
MuhammedTafli <koska_33@hotmail.com>
2022-09-29 17:01:47 +0300

    Burç bulma projesi tamamlandı.

---
+++ burc.java
@@ -0,0 +1,100 @@
+class Main {
+
+    public static void main(String[] args) {
+        java.util.Scanner klavye = new java.util.Scanner(System.in);
+        System.out.println("Doğduğunuz günü yazınız: ");
+        int gun = klavye.nextInt();
+        System.out.println("Doğduğunuz ayı yazınız: ");
+        String ay = klavye.next();
+        String burc = "";
+
+        if (ay.equalsIgnoreCase("Ocak"))
+            ;
+        {
+            if (gun > 22)
+                burc = "Kova";
+            else
+                burc = "Oğlak";
+
+        }
+        if (ay.equalsIgnoreCase("Şubat")) {
+            if (gun > 20)
+                burc = "Balık";
+            else
+                burc = "Kova";
+
+        }
+        if (ay.equalsIgnoreCase("Mart")) {
+            if (gun > 21)
+                burc = "Koç";
+            else
+                burc = "Balık";
+
+        }
+        if (ay.equalsIgnoreCase("Nisan")) {
+            if (gun > 21)
+                burc = "Boğa";
+            else
+                burc = "Koç";
+
+        }
+        if (ay.equalsIgnoreCase("Mayıs")) {
+            if (gun > 22)
+                burc = "İkizler";
+            else
+                burc = "Boğa";
+
+        }
+        if (ay.equalsIgnoreCase("Haziran")) {
+            if (gun > 23)
+                burc = "Yengeç";
+            else
+                burc = "İkizler";
+
+        }
+        if (ay.equalsIgnoreCase("Temmuz")) {
+            if (gun > 23)
+                burc = "Aslan";
+            else
+                burc = "Yengeç";
+
+        }
+        if (ay.equalsIgnoreCase("Ağustos")) {
+            if (gun > 23)
+                burc = "Başak";
+            else
+                burc = "Aslan";
+
+        }
+        if (ay.equalsIgnoreCase("Eylül")) {
+            if (gun > 23)
+                burc = "Terazi";
+            else
+                burc = "Başak";
+
+        }
+        if (ay.equalsIgnoreCase("Ekim")) {
+            if (gun > 23)
+                burc = "Akrep";
+            else
+                burc = "Terazi";
+
+        }
+        if (ay.equalsIgnoreCase("Kasım")) {
+            if (gun > 22)
+                burc = "Yay";
+            else
+                burc = "Akrep";
+
+        }
+        if (ay.equalsIgnoreCase("Aralık")) {
+            if (gun > 22)
+                burc = "Oğlak";
+            else
+                burc = "Yay";
+
+        }
+        System.out.println("Burcunuz: " + burc);
+
+    }
+}
